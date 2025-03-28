607b29be5ff8116d742d57a2c89c92ac2222573d
411211163yoyo <yc.n3x9@gmail.com>
2025-03-28 16:25:52 +0800

    commit

---
+++ index.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def job():
+    return render_template("job.html")
+
+@app.route("/future")
+def future():
+    return render_template("future.html")
+
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
